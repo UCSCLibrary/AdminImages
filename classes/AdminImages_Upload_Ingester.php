c28613a1f23fecb7176fa026d7a2afc147df4198
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-19 14:17:53 +0100

    Tidied up code

--- classes/AdminImages_Upload_Ingester.php
+++ classes/AdminImages_Upload_Ingester.php
@@ -3,30 +3,30 @@ class AdminImages_Upload_Ingester extends Omeka_File_Ingest_Upload
 {
     /**
      * Set the item to use as a target when ingesting files.
-     * 
+     *
      * @param Item $item
      * @return void
-     */        
+     */
     public function setItem(Item $item=null)
     {
-        $this->_item = null;       
+        $this->_item = null;
     }
 
     function __construct() {
         $this->setItem();
     }
-    
+
     function itemlessIngest ($fileInfo)
     {
         // Don't catch or suppress parsing errors.
         $fileInfoArray = $this->_parseFileInfo($fileInfo);
-        
+
         // Iterate the files.
         $fileObjs = array();
-        foreach ($fileInfoArray as $file) {            
-            
-            try {                
-                // This becomes the file's identifier (stored in the 
+        foreach ($fileInfoArray as $file) {
+
+            try {
+                // This becomes the file's identifier (stored in the
                 // 'original_filename' column and used to derive the archival filename).
                 $originalFileName = $this->_getOriginalFilename($file);
 
@@ -34,17 +34,17 @@ class AdminImages_Upload_Ingester extends Omeka_File_Ingest_Upload
 
                 // Create the file object.
                 if ($fileDestinationPath) {
-                    $fileMetadata = isset($file['metadata']) 
+                    $fileMetadata = isset($file['metadata'])
                         ? $file['metadata'] : array();
                     $fileObjs[] = $this->_createItemlessFile($fileDestinationPath, $originalFileName, $fileMetadata);
                 }
-            
+
             } catch (Omeka_File_Ingest_InvalidException $e) {
                 if ($this->_ignoreIngestErrors()) {
                     $this->_logException($e);
                     continue;
-                } 
-                
+                }
+
                 // If not suppressed, rethrow it.
                 throw $e;
             }
@@ -54,9 +54,9 @@ class AdminImages_Upload_Ingester extends Omeka_File_Ingest_Upload
 
  private function _createItemlessFile($newFilePath, $oldFilename, $elementMetadata = array())
     {
-        // Normally, the MIME type validator sets the type to this class's 
-        // static $mimeType property during validation. If that validator has 
-        // been disabled (from the admin settings menu, for example), set the 
+        // Normally, the MIME type validator sets the type to this class's
+        // static $mimeType property during validation. If that validator has
+        // been disabled (from the admin settings menu, for example), set the
         // MIME type here.
         if (self::$mimeType) {
             $mimeType = self::$mimeType;
@@ -67,7 +67,7 @@ class AdminImages_Upload_Ingester extends Omeka_File_Ingest_Upload
             $mimeType = $detect->detect();
         }
         $file = new File;
-        $file->item_id=0;
+        $file->item_id = 0;
         try {
             $file->original_filename = $oldFilename;
             $file->mime_type = $mimeType;
@@ -84,7 +84,3 @@ class AdminImages_Upload_Ingester extends Omeka_File_Ingest_Upload
         return $file;
     }
 }
-
-
-
-?>
