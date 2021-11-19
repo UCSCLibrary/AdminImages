0d9e27781fc5b387f5ae35bf56538d98fcc3d305
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-19 14:31:16 +0100

    Uniformed code

--- views/admin/image/edit.php
+++ views/admin/image/edit.php
@@ -1,17 +1,12 @@
 <?php
-$head = array('bodyclass' => 'admin-images primary', 
-              'title' => html_escape(__('Admin Images | Edit')));
-echo head($head);
-echo flash(); ?>
+    $head = array('bodyclass' => 'admin-images primary', 
+                  'title' => html_escape(__('Edit Admin Image #%s', $image->id))
+    );
+    echo head($head);
+?>
 
-<h2>Edit Admin Image</h2><p>
+<?php echo flash(); ?>
 
-  <?php echo $this->adminImageTag($image->id); ?>
+<?php echo $form;?>
 
-  <form method="post" action="<?php echo admin_url('admin-images/image/delete/id/').$image->id; ?>">
-    <?php echo $this->csrf;?>
-    <input type="hidden" name="image_id" value="<?php echo $image->id;?>" />
-    <input value="Delete Image"  type="submit" class="admin-image-delete" name="delete-button" id="admin-image-delete-<?php echo $image->id; ?>">
-  </form>
-  <?php echo $form;?>
-  <?php echo foot(); ?>
+<?php echo foot(); ?>
