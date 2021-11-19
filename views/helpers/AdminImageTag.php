be48b6ce5bc19bce71a2f6fe3994623c861b5f9c
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-19 14:34:39 +0100

    Added class attribute, tidied up code

--- views/helpers/AdminImageTag.php
+++ views/helpers/AdminImageTag.php
@@ -1,18 +1,20 @@
 <?php 
-class AdminImages_View_Helper_AdminImageTag extends Zend_View_Helper_Abstract
-{
-    public function adminImageTag($id,$size="fullsize")
+    class AdminImages_View_Helper_AdminImageTag extends Zend_View_Helper_Abstract
     {
-        $image = get_record_by_id("AdminImage",$id);
-        $markup = '       
-       <img class="admin-image" 
-            id="'.$image->id.'"
-            src="'.$image->getUrl($size).'"
-            alt="'.$image->alt.'"
-            title="'. $image->title.'"/>';
-        return $markup;
+        public function adminImageTag($id, $size="fullsize", $class="")
+        {
+            if ($id == '') return null;
+            
+            $image = get_record_by_id("AdminImage", $id);
+            $markup = '
+            <img  
+                id="' . $image->id . '"
+                src="' . $image->getUrl($size) . '"
+                alt="' . $image->alt . '"
+                title="' . $image->title . '"
+                class="' . $class . '"
+            />';
+            return $markup;
+        }
     }
-}
 ?>
-
-
