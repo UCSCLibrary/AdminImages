233e28d5b2e83fa55ae841e0e6b8c222b4c8b9ea
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-19 14:16:57 +0100

    Tidied up code

--- classes/AdminImageFile.php
+++ classes/AdminImageFile.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * An AdminImages Image 
- * 
+ * An AdminImages Image
+ *
  * @package AdminImages
  *
  */
@@ -13,16 +13,16 @@ class AdminImageFile
     public $thumbnail_url;
     public $square_thumbnail_url;
     public $url;
-    
+
     public function delete(){
         $db = get_db();
-        $sql = "delete from `".$db->File."` where id = ".$this->id;
+        $sql = "DELETE FROM `" . $db->File . "` WHERE id = " . $this->id;
         $result = $db->query($sql);
     }
 
     public static function FindById($id) {
         $db = get_db();
-        $sql = "select * from `".$db->File."` where id = ".$id;
+        $sql = "SELECT * FROM `" . $db->File . "` WHERE id = " . $id;
         $result = $db->query($sql);
         $image = false;
         while ($imageInfo = $result->fetch()) {
@@ -30,28 +30,29 @@ class AdminImageFile
         }
         return $image;
     }
+
     public static function FindAll() {
         $db = get_db();
-        $sql = "select * from `".$db->File."` where item_id = 0";
+        $sql = "SELECT * FROM `" . $db->File . "` WHERE item_id = 0";
         $result = $db->query($sql);
         $images = array();
         while ($imageInfo = $result->fetch()) {
             $image = self::_CreateImage($imageInfo);
-            $images[] =$image; 
+            $images[] = $image;
         }
         return $images;
     }
+
     private static function _CreateImage($imageInfo) {
         $image = new AdminImageFile;
         $image->id = $imageInfo['id'];
-        $image->filename=$imageInfo['filename'];
-        $image->original_filename=$imageInfo['original_filename'];
-        if(strpos($image->original_filename,'http')===0) {
-            $paths = explode('/',parse_url($image->original_filename,PHP_URL_PATH));
+        $image->filename = $imageInfo['filename'];
+        $image->original_filename = $imageInfo['original_filename'];
+        if(strpos($image->original_filename, 'http') === 0) {
+            $paths = explode('/', parse_url($image->original_filename, PHP_URL_PATH));
             $image->original_filename = $paths[count($paths)-1];
         }
-        $image->url=public_url('files/original/'.$imageInfo['filename']);
+        $image->url = public_url('files/original/' . $imageInfo['filename']);
         return $image;
     }
 }
-?>
