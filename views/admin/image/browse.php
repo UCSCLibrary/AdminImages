a22237936568e6d461a7e0af5d90ec70f695d981
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-19 14:32:10 +0100

    Uniformed code with core admin pages

--- views/admin/image/browse.php
+++ views/admin/image/browse.php
@@ -1,77 +1,90 @@
-<?php
+<?php echo head(array('title' => html_escape(__('Browse Admin Images') . ' ' . __('(%s total)', $total_results)))); ?>
 
-$head = array('bodyclass' => 'admin-images primary', 
-              'title' => html_escape(__('Admin Images | Browse')));
-echo head($head);
-echo flash(); 
-?>
-<h2>Browse Admin Images</h2>
-<a href="<?php echo admin_url('admin-images/image/add'); ?>"><button>Add New Image</button></a> 
-<?php if(empty($images)) : ?>
-  <h3>No admin images have been uploaded yet.</h3>
-<?php else:?>
-  <h3>Instructions</h3>
-  <h4>Shortcodes</h4>
-  <p>If you are creating a simple page or otherwise entering HTML into omeka through your browser, you can use the shortcode "[admin_image id=999,size='fullsize']" to display your admin image.</p>
-  <h4>View Helpers</h4>
-  <p>If you are creating a new view for your theme or plugin, you can use the view helper to display an Admin Image tag as follows: <code> echo $view->adminImageTag($image_id,$size="fullsize"); </code> .</p>
-  <h3>Images</h3>
-  <p></p>
-  <ul id="admin-images-list">
-    <?php foreach($images as $image) : ?>
-      <li class="admin-image">
-        <table>
-          <tr>
-            <td>Original:</td>
-            <td><a href="<?php echo $image->getUrl('original');?>"><?php echo $image->getUrl('original');?></a></td>
-          </tr><tr>
-            <td>Fullsize:</td>
-            <td><a href="<?php echo $image->getUrl('fullsize');?>"><?php echo $image->getUrl('fullsize');?></a></td>
-          </tr><tr>
-            <td>Square Thumbnail:</td>
-            <td><a href="<?php echo $image->getUrl('square_thumbnail');?>"><?php echo $image->getUrl('square_thumbnail');?></a></td>
-          </tr><tr>
-            <td>Thumbnail:</td>
-            <td><a href="<?php echo $image->getUrl('thumbnail');?>"><?php echo $image->getUrl('thumbnail');?></a></td>
-          </tr>
+<?php echo flash(); ?>
+
+<a class="add-image button green" href="<?php echo admin_url('admin-images/index/add'); ?>"><?php echo __('Add an Image'); ?></a>
+
+<?php if (count($images)): ?>
+    <?php echo pagination_links(); ?>
+    <div class="table-responsive">
+        <table id="admin-images-list">
+            <thead>
+                <tr>
+                    <th class='id'>ID</th>
+                    <?php
+                        echo browse_sort_links(
+                            array(
+                                __('Title') => 'title',
+                                __('Permalink') => 'href',
+                                __('Added by') => 'owner_id'
+                            ),
+                            array('link_tag' => 'th scope="col"', 'list_tag' => '')
+                        );
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $key = 0; ?>
+                <?php foreach ($images as $image): ?>
+                    <tr class="admin-image-tr <?php echo (++$key%2 == 1 ? 'odd' : 'even'); ?>">
+                        <td class='id'>
+                            <?php
+                                echo $image->id;
+                            ?>
+                        </td>
+                        <td class="admin-image-info">
+                            <?php
+                                echo "<img src='" . $image->getUrl('thumbnail') . "'>";
+                                echo "<strong>" . $image->title . "</strong>";
+                            ?>
+                            <ul class="action-links group">
+                                <li><a class="edit-image" href="<?php echo admin_url('admin-images/index/edit/id/') . $image->id; ?>"><?php echo __('Edit'); ?></a></li>
+                                <li><a class="delete-image delete-confirm" href="<?php echo admin_url('admin-images/index/delete-confirm/id/') . $image->id; ?>"><?php echo __('Delete'); ?></a></li>
+                            </ul>
+                            <div class="details">
+                            <?php
+                                $url = $image->getUrl('original');
+                                if ($size = getimagesize($url)) {
+                                    echo "<p><strong>" . __('Width') . "</strong>: " . $size[0] . "px</p>";
+                                    echo "<p><strong>" . __('Height') . "</strong>: " . $size[1] . "px</p>";
+                                    echo "<p><strong>" . __('MIME type') . ":</strong> " . $size['mime'] . "</p>";
+                                }
+                                $altText = $image->alt;
+                                if ($altText != '') echo "<p><strong>" . __('Alt text') . ":</strong> " . $altText . "</p>";
+                                echo "<p><strong>" . __('Direct links') . "</strong>: ";
+                                echo "<a href='" . $url . "' target='_blank'>" . __('Original') . "</a> ";
+                                echo "<a href='" . $image->getUrl('fullsize') . "' target='_blank'>" . __('Fullsize') . "</a> ";
+                                echo "<a href='" . $image->getUrl('thumbnail') . "' target='_blank'>" . __('Thumbnail') . "</a> ";
+                                echo "<a href='" . $image->getUrl('square_thumbnail') . "' target='_blank'>" . __('Square thumbnail') . "</a></p>";
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo $image->href;
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo $image->getUsername();
+                            ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
         </table>
+    </div>
 
-        <div class="admin-image-thumb">
-          <?php echo $image->linkToImage('fullsize');?>
-        
-          <div class="admin-image-meta">
-            Image ID:
-            <?php echo $image->id; ?>
-          </div>
+    <script type="text/javascript">
+        Omeka.addReadyCallback(Omeka.AdminImages.setupDetails, [
+            <?php echo js_escape(__('Details')); ?>,
+            <?php echo js_escape(__('Show Details')); ?>,
+            <?php echo js_escape(__('Hide Details')); ?>
+        ]);
+    </script>
+<?php else: ?>
+    <p>
+        <?php echo __('There seems to be no Admin Images stored in the repository.') . ' <a class="add-image" href="' . admin_url('admin-images/index/add') . '">' . __('Add an Image') . '</a>.'; ?>
+    </p>
+<?php endif; ?>
 
-        
-          <div class="admin-image-meta">
-            Title:
-            <?php echo $image->title; ?>
-          </div>
-
-          <div class="admin-image-meta">
-            Alt text:
-            <?php echo $image->alt; ?>
-          </div>
-
-          <div class="admin-image-meta">
-            Image default link:
-            <?php echo $image->href; ?>
-          </div>
-
-          <a class="edit" href="<?php echo(admin_url('admin-images/image/edit/id/').$image->id); ?>"><button>Edit</button></a>
-          <form method="post"  action="<?php echo admin_url('admin-images/image/delete/id/').$image->id; ?>">
-            <?php echo $this->csrf;?>
-            <input type="hidden" name="image_id" value="<?php echo $image->id;?>" />
-            <input value="Delete"  type="submit" class="admin-image-delete" name="delete-button" id="admin-image-delete-<?php echo $image->id; ?>">
-          </form>
-
-        </div>
-      </li>
-    <?php endforeach; ?>
-  </ul>
-<?php endif;?>
-<?php
-echo foot(); 
-?>
+<?php echo foot(); ?>
