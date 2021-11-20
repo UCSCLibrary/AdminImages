d6f10cc817a516b1f69919abcd8ce29e8884b8d5
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-20 09:01:48 +0100

    Added image details field (text only)

--- forms/EditFileForm.php
+++ forms/EditFileForm.php
@@ -30,24 +30,29 @@ class Admin_Images_Edit_Form extends Omeka_Form_Admin
      */
     private function _registerElements()
     { 
+        $this->addElementToEditGroup('note', 'details', array(
+            'label'         => __('Details'),
+            'order'         => 1
+        ));
+        
         $this->addElementToEditGroup('text', 'title', array(
             'label'         => __('Title'),
             'description'   => __('The title of the image. Will be used as tooltip on mouse-over, and to identify the image in the Admin interface.'),
-            'order'         => 1,
+            'order'         => 2,
             'required'      => true
         ));
 
         $this->addElementToEditGroup('text', 'alt', array(
             'label'         => __('Alt Text'),
             'description'   => __('The alternative text displayed when the image does not get loaded on a user\'s browser.'),
-            'order'         => 2,
+            'order'         => 3,
             'required'      => true
         ));
 
         $this->addElementToEditGroup('text', 'href', array(
             'label'         => __('Permalink'),
             'description'   => __('The URL to which the image is linking to. To be used only if the image is permanently associated with a Simple Page, Item, Collection or Exhibit.'),
-            'order'         => 3
+            'order'         => 4
         ));
 
         if (version_compare(OMEKA_VERSION, '2.2.1') >= 0) {
