761e6129abc9004636640eb7c62320cf7540f623
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-07-19 09:36:10 +0200

    Update AdminImageTag.php
    Tidied up code

--- views/helpers/AdminImageTag.php
+++ views/helpers/AdminImageTag.php
@@ -1,15 +1,24 @@
-<?php
-class AdminImages_View_Helper_AdminImageTag extends Zend_View_Helper_Abstract
-{
-    public function adminImageTag($id,$size="fullsize")
-    {
-        $image = get_record_by_id("AdminImage",$id);
-        $markup = '       
-       <img class="admin-image" 
-            id="'.$image->id.'"
-            src="'.$image->getUrl($size).'"
-            alt="'.$image->alt.'"
-            title="'. $image->title.'"/>';
-        return $markup;
-    }
-}
+<?php 
+	class AdminImages_View_Helper_AdminImageTag extends Zend_View_Helper_Abstract
+	{
+		public function adminImageTag($id, $size="fullsize", $class="")
+		{
+			if ($id == '') return null;
+			
+			$image = get_record_by_id("AdminImage", $id);
+			$markup = '
+				<img  
+					 id="' . $image->id . '"
+					 src="' . $image->getUrl($size) . '"
+					 alt="' . ($image->alt != '' ? $image->alt : 'Admin Image') . '"
+					 title="' . $image->title . '"
+					 class="' . $class . '"
+				/>
+			';
+			
+			if ($image->href != '') $markup = '<a href="' . $image->href . '" target="_blank">' . $markup . '</a>';
+			
+			return $markup;
+		}
+	}
+?>
