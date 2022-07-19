2767143f53e6fb4c66ee53d07043d70ae1b780be
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-07-19 09:35:54 +0200

    Update AdminImageTag.php
    Tidied up code

--- views/helpers/AdminImageTag.php
+++ views/helpers/AdminImageTag.php
@@ -1,22 +1,24 @@
 <?php 
-    class AdminImages_View_Helper_AdminImageTag extends Zend_View_Helper_Abstract
-    {
-        public function adminImageTag($id, $size="fullsize", $class="")
-        {
-            if ($id == '') return null;
-            
-            $image = get_record_by_id("AdminImage", $id);
-            $markup = '
-            <img  
-                id="' . $image->id . '"
-                src="' . $image->getUrl($size) . '"
-                alt="' . ($image->alt != '' ? $image->alt : 'Admin Image') . '"
-                title="' . $image->title . '"
-                class="' . $class . '"
-            />';
+	class AdminImages_View_Helper_AdminImageTag extends Zend_View_Helper_Abstract
+	{
+		public function adminImageTag($id, $size="fullsize", $class="")
+		{
+			if ($id == '') return null;
 			
-            if ($image->href != '') $markup = '<a href="' . $image->href . '" target="_blank">' . $markup . '</a>';
+			$image = get_record_by_id("AdminImage", $id);
+			$markup = '
+				<img  
+					 id="' . $image->id . '"
+					 src="' . $image->getUrl($size) . '"
+					 alt="' . ($image->alt != '' ? $image->alt : 'Admin Image') . '"
+					 title="' . $image->title . '"
+					 class="' . $class . '"
+				/>
+			';
 			
-            return $markup;
-        }
-    }
+			if ($image->href != '') $markup = '<a href="' . $image->href . '" target="_blank">' . $markup . '</a>';
+			
+			return $markup;
+		}
+	}
+?>
