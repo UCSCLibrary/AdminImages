2c1deb335748a2e8c2aa52329ea55e1779b8cdfd
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-08-04 01:01:00 +0200

    Added ability to delete file (with derivatives) when removing Image

--- classes/AdminImageFile.php
+++ classes/AdminImageFile.php
@@ -14,10 +14,89 @@ class AdminImageFile
     public $square_thumbnail_url;
     public $url;
 
-    public function delete(){
+    /**
+     * File extension for all image derivatives.
+     */
+    const DERIVATIVE_EXT = 'jpg';
+
+    /**
+     * Folder paths for each type of files/derivatives.
+     *
+     * @var array
+     */
+    private static $_pathsByType = array(
+        'original' => 'original',
+        'fullsize' => 'fullsize',
+        'thumbnail' => 'thumbnails',
+        'square_thumbnail' => 'square_thumbnails'
+    );
+
+    public function delete() {
+		self::_unlinkFile();
+		
         $db = get_db();
         $sql = "DELETE FROM `" . $db->File . "` WHERE id = " . $this->id;
         $result = $db->query($sql);
+    }
+	
+	/**
+     * Unlink the file and file derivatives belonging to this record.
+     */
+    public function _unlinkFile() {
+        $storage = $this->getStorage();
+        $files = array($this->getStoragePath('original'));
+		$types = self::$_pathsByType;
+		unset($types['original']);
+		foreach ($types as $type => $path) {
+			$files[] = $this->getStoragePath($type);
+		}
+        foreach ($files as $file) {
+            $storage->delete($file);
+        }
+    }
+
+    /**
+     * Get the storage object.
+     * 
+     * @return Omeka_Storage
+     */
+    public function getStorage()
+    {
+        if (!$this->_storage) {
+            $this->_storage = Zend_Registry::get('storage');
+        }
+        return $this->_storage;
+    }
+
+    /**
+     * Get a storage path for the file.
+     * 
+     * @param string $type
+     * @return string
+     */
+    public function getStoragePath($type = 'original')
+    {
+        $storage = $this->getStorage();
+        if ($type == 'original') {
+            $fn = $this->filename;
+        } else {
+            $fn = $this->getDerivativeFilename();
+        }
+        if (!isset(self::$_pathsByType[$type])) {
+            throw new RuntimeException(__('"%s" is not a valid file derivative.', $type));
+        }
+        return $storage->getPathByType($fn, self::$_pathsByType[$type]);
+    }
+
+    /**
+     * Get the filename for this file's derivative images.
+     * 
+     * @return string
+     */
+    public function getDerivativeFilename()
+    {
+        $base = pathinfo($this->filename, PATHINFO_EXTENSION) ? substr($this->filename, 0, strrpos($this->filename, '.')) : $this->filename;
+        return $base . '.' . self::DERIVATIVE_EXT;
     }
 
     public static function FindById($id) {
@@ -48,7 +127,7 @@ class AdminImageFile
         $image->id = $imageInfo['id'];
         $image->filename = $imageInfo['filename'];
         $image->original_filename = $imageInfo['original_filename'];
-        if(strpos($image->original_filename, 'http') === 0) {
+        if (strpos($image->original_filename, 'http') === 0) {
             $paths = explode('/', parse_url($image->original_filename, PHP_URL_PATH));
             $image->original_filename = $paths[count($paths)-1];
         }
